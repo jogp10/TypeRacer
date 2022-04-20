cb02a8f4f606d8882a8c9726a827e5b00fad5676
João Pinheiro <jogp10@gmail.com>
2022-04-20 23:30:34 +0100

    lab2 first part

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,25 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if(lsb==NULL) return 1;
+  *lsb = (uint8_t) val;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if(msb==NULL) return 1;
+  *msb = (uint8_t) (val >> 8);
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint32_t var;
 
-  return 1;
+  if(sys_inb(port, &var)) {
+    printf("Error reading byte\n");
+    return 1;
+  };
+
+  *value = var & 0xFF;
+  return 0;
 }
