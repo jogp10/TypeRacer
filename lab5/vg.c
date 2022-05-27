f0f438948431cc9d8314f35fd47deb0adfa55c00
João Pinheiro <jogp10@gmail.com>
2022-05-27 20:21:52 +0100

    som refactor

--- lab5/vg.c
+++ lab5/vg.c
@@ -16,9 +16,6 @@ int (vg_change_mode)(uint16_t mode) {
         printf("Error getting information on mode.\n");
         return 1;
     }
-    h_res = info.XResolution;
-    v_res = info.YResolution;
-    bits_per_pixel = info.BitsPerPixel;
 
     // Map memory
     if (map_memory()) {
@@ -55,10 +52,9 @@ int (map_memory)() {
     struct minix_mem_range mr;
     unsigned int vram_base = info.PhysBasePtr;
     unsigned int vram_size = v_res * h_res * (ceil(bits_per_pixel / 8));
-    uint16_t r;
+    int r;
 
     /* Allow memory mapping */
-
     mr.mr_base = (phys_bytes) vram_base;	
     mr.mr_limit = mr.mr_base + vram_size;  
 
@@ -68,7 +64,6 @@ int (map_memory)() {
     }
 
     /* Map memory */
-
     video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vram_size);
 
     if(video_mem == MAP_FAILED) {
@@ -98,25 +93,30 @@ int (vbe_get_info_mode)(uint16_t mode, vbe_mode_info_t *info) {
 
 
   /* Make the BIOS call */
-  if (sys_int86(&r) != OK) {
+  if (sys_int86(&r)) {
     printf("vc_get_mode_info: sys_int86() failed \n");
     return 1;
   }
 
     /* Function not supported or succesful */
-  if (r.ah != 0x00 || r.al != 0x4F) {
+  if (r.ah != VBE_SUCCESS || r.al != VBE_SUPPORTED) {
       printf("Error in function call.\n");
       lm_free(&mem_map);
       return 1;
   }
 
   *info = *(vbe_mode_info_t *)mem_map.virt;
+
+  h_res = info->XResolution;
+  v_res = info->YResolution;
+  bits_per_pixel = info->BitsPerPixel;
+
   lm_free(&mem_map);
   return 0;
 }
 
 int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color) {
-    for (int i = 0; i < height; i++) {
+    for (unsigned i = 0; i < height; i++) {
         if (vg_draw_hline(x, y + i, width, color)) {
             printf("Error drawing line.\n");
             return 1;
@@ -137,14 +137,15 @@ int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t width, uint32_t color) {
 
 int (vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color) {
     if (x < 0 || x > h_res || y > v_res || y < 0 ) return 0;
-    uint8_t *pixel = (uint8_t *) video_mem + ( (y * h_res) + x ) * (int) ceil(bits_per_pixel / 8);
+
+    uint8_t *pixel = (uint8_t *) video_mem + (( (y * h_res) + x ) * (int) ceil(bits_per_pixel / 8));
 
     for (int i = 0; i < (int) ceil(bits_per_pixel / 8); i++) {
         *pixel = color;
         color >>= 8;
         pixel++;
     }
-    
+
     return 0;
 }
 
